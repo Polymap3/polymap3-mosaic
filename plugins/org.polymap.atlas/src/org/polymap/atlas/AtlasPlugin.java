d9d852d4579469128495b364718e22b16a65e546
fb71 <falko@polymap.de>
2013-01-30 15:29:24 +0100

    #3: Portal-UI Infrastruktur (https://polymap.org/mosaic/ticket/3)   - NEW: org.polymap.atlas: der neue Atlas-Client   - erster Check-in zum Prüfen der Git-Infrastruktur

---
+++ plugins/org.polymap.atlas/src/org/polymap/atlas/AtlasPlugin.java
@@ -0,0 +1,40 @@
+/* 
+ * polymap.org
+ * Copyright 2013, Falko Bräutigam. All rights reserved.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3.0 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ */
+package org.polymap.atlas;
+
+import org.osgi.framework.BundleContext;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+/**
+ * 
+ *
+ * @author <a href="http://www.polymap.de">Falko Bräutigam</a>
+ */
+public class AtlasPlugin
+        extends AbstractUIPlugin {
+
+    @Override
+    public void start( BundleContext context ) throws Exception {
+        super.start( context );
+    }
+
+    
+    @Override
+    public void stop( BundleContext context ) throws Exception {
+        super.stop( context );
+    }
+    
+}
